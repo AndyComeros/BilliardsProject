80cc48047621048c46a089c80ed7b9dab23cc5f9
Idraliski <ultra4423@hotmail.com>
2022-06-03 17:37:20 +0800

    took out comments

--- BilliardsProject/Object.c
+++ BilliardsProject/Object.c
@@ -151,13 +151,9 @@ void drawSphereObject(Object* obj)
 	
 	glTranslatef(obj->body.position.x, obj->body.position.y, obj->body.position.z);
 
-
 	drawSphereShadow(20, 2, 0, -2.2,0);
-	//dont know why but when velocity is 0 and a rotation is made the ball dissapears. so this check is here i guess...
-	//if (length(obj->body.velocity) > 0) {
-		glRotatef(obj->body.rotAngle, obj->body.rotation.z, obj->body.rotation.y, obj->body.rotation.x);
-	//}
-	
+
+	glRotatef(obj->body.rotAngle, obj->body.rotation.z, obj->body.rotation.y, obj->body.rotation.x);
 	glScalef(obj->body.scale.x, obj->body.scale.y, obj->body.scale.z);
 
 	if (ballMode == 0)
